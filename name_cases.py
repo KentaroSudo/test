4974efd9026bfb1024a8f082eb271afaed75b232
KentaroSudo <ksudo91@gmail.com>
2022-05-04 19:18:09 +0900

    commit

---
+++ name_cases.py
@@ -0,0 +1,12 @@
+person_name = "Eric"
+message = f"Hello {person_name}, would you like to learn some Python today?"
+print(message)
+lower_case_name = person_name.lower()
+upper_case_name = person_name.upper()
+title_case_name = person_name.title()
+print(lower_case_name,"\n",upper_case_name,"\n",title_case_name)
+
+author_name = "albert einsterin"
+author_quotation = " \"A person who never made a mistake never tried anything new\" "
+message_author = f"{author_name.title()} onece said,{author_quotation}"
+print(message_author)
---
+++ scraping.py
@@ -0,0 +1,9 @@
+import urllib.request, urllib.error
+
+page = urllib.request.urlopen('https://www.wunderground.com/history/daily/KBUF/date/2009-1-1')
+
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(page,features="html.parser")
+nobrs = soup.find_all(attrs={"class":"nobr"})
+print(nobrs)
